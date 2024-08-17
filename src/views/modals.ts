0ef0d9c186c83dc739bf6d28d7b7dc48c1816c3d
Jun Young Bang <bhang96@gmail.com>
2024-08-18 00:25:39 +0800

    style: Update GeuloModal width to 100% for better responsiveness

--- src/views/modals.ts
+++ src/views/modals.ts
@@ -3,11 +3,13 @@ import { App, Modal } from "obsidian";
 export class GeuloModal extends Modal {
 	title: string;
 	message: string;
+	
 	constructor(app: App, title: string, message: string) {
 		super(app);
 		this.title = title;
 		this.titleEl.setText(this.title);
 		this.message = message;
+		this.modalEl.style.width = '100%';
 	}
 
 	onOpen() {
